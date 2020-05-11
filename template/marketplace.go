58cc24f8b96ffd969dac014e2e209179c92401dd
Munzir <munzirmz36@gmail.com>
2020-05-11 16:38:00 +0700

    fix bug marketplace

--- template/marketplace.go
+++ template/marketplace.go
@@ -9,12 +9,13 @@ func MarketplaceTemplate(w http.ResponseWriter, r *http.Request) {
 	var tmpl = ParseFiles("marketplace.html")
 	session, _ := controller.Store.Get(r, "session")
 	username, notok := session.Values["username"]
+	if !notok {
+		http.Redirect(w, r, "/signin", http.StatusSeeOther)
+		return
+	}
 	var data = map[string]interface{}{
 		"login": true,
 		"name":  username.(string),
-	}
-	if !notok {
-		data["login"] = false
 	}
 	var err = tmpl.ExecuteTemplate(w, "marketplace", data)
 	if err != nil {
